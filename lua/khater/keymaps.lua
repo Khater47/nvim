bf36d55db9b7e19241d101ac15561699dc7942f9
Ahmed Khater <ahmed.m.khater89@gmail.com>
2025-03-21 16:08:52 +0100

    Keymaps

--- lua/khater/keymaps.lua
+++ lua/khater/keymaps.lua
@@ -1,1 +1,26 @@
+-- Remaping options
+local opts = { noremap = true, silent = true}
 
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+-- Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Lexplorer
+keymap("n", "<leader>e", ":Lex 20<cr>", opts)
+
+-- Buffer navigation
+keymap("n", "<S-l>", ":bnext<cr>", opts) -- Next buffer
+keymap("n", "<S-h>", ":bprevious<cr>", opts) -- Previous buffer
+
+-- Fast ESC
+keymap("i", "jk", "<ESC>", opts)
+
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
